0571813264cd2aa0439d8bc948a125b9b69880b3
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-26 13:49:17 -0500

    exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,12 @@
 public class ControlFlowExercises {
     public static void main (String[] arg){
-
-        int i = 5;
-
-        while (i <= 15){
-            System.out.println(i);
-            i++;
-        }
+        int i = 0;
+        do {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i += 2;
+        }while(i <= 100);
 
     }
 
