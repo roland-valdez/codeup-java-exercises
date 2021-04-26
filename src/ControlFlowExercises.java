44b867ccf32903b59cc644b37bba8928dfcc3b51
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-26 14:10:00 -0500

    exercise 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,23 @@
+
 public class ControlFlowExercises {
     public static void main (String[] arg){
-//        double i = 2;
-//        do {
-//                System.out.printf("\n%.0f",i);
-//            i = i * i;
-//        }while(i <= 1000000);
+//
 
-        for (double i = 2; i <= 1000000; i = i * i){
-            System.out.printf("\n%.0f",i);
+        for (int i = 1; i <= 100; i++){
+
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+
         }
     }
 
