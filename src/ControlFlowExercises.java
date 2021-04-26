e2959c93e971e899c77ec9c42adc293f2f87f035
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-26 14:32:35 -0500

    exercise 3 refactored

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,22 +3,26 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main (String[] arg){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What number would you like to go up to:");
-        int numberTo = scanner.nextInt();
-        System.out.println("Here is your table");
-        System.out.println("number | squared | cubed");
-        System.out.println("------ | ------- | -----");
-        for (int i = 1; i <= numberTo; i++) {
-            int square = (i * i);
-            int cubed = (i * i * i);
-            if (square > 9) {
-                System.out.printf("%d      | %d      | %d\n", i, square, cubed);
-            }
-            else {
-                System.out.printf("%d      | %d       | %d\n", i, square, cubed);
-            }
-        }
+        String confirm;
+        do {
 
+            System.out.println("What number would you like to go up to:");
+            int numberTo = scanner.nextInt();
+            System.out.println("Here is your table");
+            System.out.println("number | squared | cubed");
+            System.out.println("------ | ------- | -----");
+            for (int i = 1; i <= numberTo; i++) {
+                int square = (i * i);
+                int cubed = (i * i * i);
+                if (square > 9) {
+                    System.out.printf("%d      | %d      | %d\n", i, square, cubed);
+                } else {
+                    System.out.printf("%d      | %d       | %d\n", i, square, cubed);
+                }
+            }
+            System.out.println("Do you wish to continue?: Y/y");
+            confirm = scanner.next();
+        }while(confirm.equalsIgnoreCase("y"));
 
     }
 
