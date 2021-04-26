e8c779e93642dce79e39925e86106a89b2837910
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-26 13:40:38 -0500

    exercise 1

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,13 @@
+public class ControlFlowExercises {
+    public static void main (String[] arg){
+
+        int i = 5;
+
+        while (i <= 15){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main (String[] args){
+        int x = 1;
+        int y = 1;
+        int z = 0;
+
+
+        if (true & false){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
