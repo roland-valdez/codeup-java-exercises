3758cb93cdd78cbc62ee1ed4491d7e89f92ce70b
Roland Valdez <valdez.roland.23@gmail.com>
2021-04-28 23:50:18 -0500

    Exercise 1

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String firstLyric = "We don't need no education";
+        String secondLyric = "We don't need no thought control";
+        System.out.println(firstLyric);
+        System.out.println(secondLyric);
+        System.out.println("Check \"this\" out!, \"s inside of \"s!");
+        System.out.println("In windows, the main drive is usually C:\\");
+        System.out.println("I can do \\, double backlashes \\\\,\nand the amazing triple backslash\\\\\\!");
+
+    }
+
+}
