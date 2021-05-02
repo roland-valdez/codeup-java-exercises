6c627294766ac5856ff1712892b39d2d1779e6a6
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-02 15:22:05 -0500

    bonus exercise 2

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,14 +2,26 @@ package shapes;
 
 import util.Input;
 
+import java.util.Scanner;
+
 public class CircleApp {
 
     public static void main(String[] args){
-        System.out.println("To calculate circle properties I am going to need the radius of the circle.");
-        Input circleRadius = new Input();
-        Circle setRadius = new Circle(circleRadius.getDouble());
-        System.out.println("The area of the the circle is " + setRadius.getArea());
-        System.out.println("The circumference of the the circle is " + setRadius.getCircumference());
-
+        Scanner scanner = new Scanner(System.in);
+        boolean confirm = true;
+        int i = 0;
+        do {
+            System.out.println("To calculate circle properties I am going to need the radius of the circle.");
+            Input circleRadius = new Input();
+            Circle setRadius = new Circle(circleRadius.getDouble());
+            System.out.println("The area of the the circle is " + setRadius.getArea());
+            System.out.println("The circumference of the the circle is " + setRadius.getCircumference());
+            System.out.println("Would you like to do another circle calculation? y/n");
+            if (!scanner.nextLine().toLowerCase().startsWith("y")){
+                confirm = false;
+            }
+            i++;
+        } while (confirm);
+        System.out.println("You ran the calculations for circles " + i + " times.");
     }
 }
