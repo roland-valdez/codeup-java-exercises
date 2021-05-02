517d372d9423050b4b18cf005b87dab39e8754c2
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-01 23:58:21 -0500

    working on exercise 2

---
+++ src/shapes/Circle.java
@@ -0,0 +1,15 @@
+package shapes;
+
+public class Circle {
+
+    private double radius;
+    public Circle(double radius){
+
+    }
+    public double getArea(){
+        return 2;
+    }
+    public double getCircumference(){
+        return 2;
+    }
+}
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,10 +4,10 @@ public class InputTest {
     public static void main (String[] args) {
         Input input = new Input();
         System.out.println(input.yesNo());
-        System.out.println(input.getString());
-//        System.out.println(input.getInt());
+        System.out.println(input.getString());//needs work
+//        System.out.println(input.getInt());//needs work
         System.out.println(input.getInt(1, 10));
-//        System.out.println(input.getDouble());
+//        System.out.println(input.getDouble());//needs work
         System.out.println(input.getDouble(1, 10));
 
     };
