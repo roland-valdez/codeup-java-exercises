ddbd7ae0a99c8831df1e2677f61f23f586235300
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-04 16:00:58 -0500

    finished exercise

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,15 @@
 package shapes;
 
 public class Square extends Rectangle {
+    protected double side = 5;
     public Square (double side){
         super(side, side);
     }
 
+    public double getArea(){
+        return Math.pow(side, 2);
+    }
+    public double getPerimeter(){
+        return this.side * 4;
+    }
 }
