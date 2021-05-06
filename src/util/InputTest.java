9fca815f7ad2624a4b093977c29330fc97d52000
Roland Valdez <valdez.roland.23@gmail.com>
2021-05-05 23:08:59 -0500

    refactored Input.java as it was missing private property

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,14 +3,11 @@ package util;
 public class InputTest {
     public static void main (String[] args) {
         Input input = new Input();
-//        System.out.println(input.yesNo());
-//        System.out.println(input.getInt(1, 10));
-//        System.out.println(input.getString());//needs work
+        System.out.println(input.yesNo());
+        System.out.println(input.getInt(1, 10));
+        System.out.println(input.getString());//needs work
         System.out.println(input.getInt());//needs work
-//
-//        System.out.println(input.getDouble());//needs work
-//        System.out.println(input.getDouble(1, 10));
-
+        System.out.println(input.getDouble());//needs work
+        System.out.println(input.getDouble(1, 10));
     };
-
 };
